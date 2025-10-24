e38938d284a00dc4d7efb65a4ef97e1f2cfcfb41
prosany <mahabubsany2@gmail.com>
2025-10-24 15:17:08 +0600

    Remove duplicate console log for app initialization

--- src/libs/initiateServer.ts
+++ src/libs/initiateServer.ts
@@ -2,4 +2,5 @@ import express, { Express } from 'express';
 import { createServer } from 'http';
 
 export const app: Express = express();
+console.log('🌺 | app:', app);
 export const server = createServer(app);
