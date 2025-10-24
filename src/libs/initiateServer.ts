9d34d165244ebaa38dd0230912c143fa8dcb565a
prosany <mahabubsany2@gmail.com>
2025-10-24 16:29:48 +0600

    fix: correct syntax error in app initialization

--- src/libs/initiateServer.ts
+++ src/libs/initiateServer.ts
@@ -1,7 +1,7 @@
 import express, { Express } from 'express';
 import { createServer } from 'http';
 
-export const app: Express = express(;
+export const app: Express = express;
 console.log('🌺 | app:', app);
 
 export const server = createServer(app);
